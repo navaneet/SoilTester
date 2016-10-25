b04fa9301f376e54a33cc50eac52d69176bc591b
Navaneet Sarma <sukhoi30mkin@gmail.com>
2016-10-26 02:50:25 +0530

    Initial Commit

---
+++ SoilTester/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SoilTester
+//
+//  Created by Navaneet Sarma on 26/10/16.
+//  Copyright © 2016 Navaneet Sarma. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SoilTester/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SoilTester
+//
+//  Created by Navaneet Sarma on 26/10/16.
+//  Copyright © 2016 Navaneet Sarma. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
